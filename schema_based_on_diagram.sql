e1b75a23fcde2da494a1dcd0c2588bb818b2f032
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-07 18:22:38 +0300

    Update created the prescribed_treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS medical_histories (
   patient_id INT NOT NULL,
   status VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
---   CONSTRAINT patients_id_fk FOREIGN KEY(id) REFERENCES patients(id),
+  CONSTRAINT patients_id_fk FOREIGN KEY(id) REFERENCES patients(id),
 --   CONSTRAINT patients_id_fk FOREIGN KEY(patient_id) REFERENCES prescribed_treatments(id)
 );
 
@@ -20,5 +20,11 @@ CREATE TABLE IF NOT EXISTS treatments(
   treatments_id INT,
   type VARCHAR(255),
   name VARCHAR(255),
-  -- CONSTRAINT patients_id_fk FOREIGN KEY(id) REFERENCES patients(id),
+  -- CONSTRAINT patients_id_fk FOREIGN KEY(id) REFERENCES prescribed_treatments(patient_id)
+);
+
+CREATE TABLE IF NOT EXISTS prescribed_treatments(
+  treatments_id INT,
+  patient_id INT,
+  PRIMARY KEY(treatments_id, patient_id)
 );
