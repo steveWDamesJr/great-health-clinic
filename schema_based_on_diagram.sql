23d97885b94c2da6472d054c3a35a4b668c2d982
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-08 11:49:22 +0300

    Update the trclinics schema to avoid using un-necessary alter clause

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,31 +14,21 @@ CREATE TABLE IF NOT EXISTS medical_histories (
   CONSTRAINT patients_id_fk FOREIGN KEY(id) REFERENCES patients(id)
 );
 
-CREATE TABLE IF NOT EXISTS treatments(
-  id INT GENERATED ALWAYS AS IDENTITY,
-  treatments_id INT,
-  type VARCHAR(255),
-  name VARCHAR(255)
--- ALTER TABLE treatments ADD CONSTRAINT treatments_patient_id_fk FOREIGN KEY(treatments_id) REFERENCES prescribed_treatments(treatments_id);
-);
-
-CREATE TABLE IF NOT EXISTS prescribed_treatments (
-  id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
-  treatments_id INT,
-  medical_history INT,
-  PRIMARY KEY (treatments_id, medical_history)
-);
-
--- Update made the patient_id a foreign key referencing the prescribed_treatments table
-ALTER TABLE medical_histories ADD CONSTRAINT medical_patient_id_fk FOREIGN KEY(patient_id) REFERENCES prescribed_treatments(patient_id);
-
 CREATE TABLE IF NOT EXISTS invoices (
   id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
   total_amount DECIMAL NOT NULL,
   generated_at timestamp NOT NULL,
   payed_at timestamp NOT NULL,
-  medical_history_id INT NOT NULL,
-  PRIMARY KEY (id,generated_at,payed_at)
+  medical_history_id INT NOT NULL UNIQUE,
+  PRIMARY KEY (id,generated_at,payed_at),
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
+);
+
+CREATE TABLE IF NOT EXISTS treatments(
+  id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+  treatments_id INT,
+  type VARCHAR(255),
+  name VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS invoice_items (
@@ -48,10 +38,16 @@ CREATE TABLE IF NOT EXISTS invoice_items (
   total_price DECIMAL NOT NULL,
   invoice_id INT NOT NULL,
   treatment_id INT NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
 
-
-ALTER TABLE invoice_items ADD CONSTRAINT invoice_id_fk FOREIGN KEY(invoice_id) REFERENCES invoices(id);
-
-ALTER TABLE invoices ADD CONSTRAINT invoices_medical_histories_fk FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id);
+CREATE TABLE IF NOT EXISTS prescribed_treatments (
+  id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+  treatments_id INT,
+  medical_history_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (treatments_id) REFERENCES treatments (id),
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
+);
