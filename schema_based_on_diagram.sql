818639f578e669fc676fb723cb6e5c2fd59c0c29
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-06-08 04:00:35 -0500

    Add indexs for tables
    Add index to:
    -medical histories table
    -invoices table
    -invoice_items table
    -prescribed treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -51,3 +51,11 @@ CREATE TABLE IF NOT EXISTS prescribed_treatments (
   FOREIGN KEY (treatments_id) REFERENCES treatments (id),
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
 );
+
+
+CREATE INDEX patient_medic_history_id ON medical_histories(patient_id);
+CREATE INDEX invoice_medic_history_invoice ON invoices(medical_history_id);
+CREATE INDEX invoice_items_id_index ON invoice_items(invoice_id);
+CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
+CREATE INDEX treatments_id_index_pres_treat  ON prescribed_treatments(treatments_id);
+CREATE INDEX medi_history_prescribed_treatments ON prescribed_treatments(medical_history_id);
